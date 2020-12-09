8c06066f1c961354aec5710071f96d1a7db2db9f
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-12-09 12:01:12 +0300

    ping once implemented

--- CustomPing/CustomPing.cpp
+++ CustomPing/CustomPing.cpp
@@ -8,13 +8,10 @@
 #pragma comment (lib, "IPHLPAPI.lib")
 #pragma comment (lib, "Ws2_32.lib")
 
+using namespace std;
 
 int hostname_to_ip_address(const char *hostname, in_addr *ipv4_addr)
 {
-    
-    WSADATA wsa_startup_result;
-    WSAStartup(MAKEWORD(1, 1), &wsa_startup_result);
-
     const ADDRINFO hints =
     {
         AI_CANONNAME,
@@ -28,8 +25,24 @@ int hostname_to_ip_address(const char *hostname, in_addr *ipv4_addr)
     return GetLastError();
 }
 
+int ping(const char *hostname, int timeout, ICMP_ECHO_REPLY *reply)
+{ 
+    in_addr* addr = new in_addr();
+    hostname_to_ip_address(hostname, addr);
+
+    HANDLE icmp_handle = IcmpCreateFile();
+    int reply_size = sizeof(ICMP_ECHO_REPLY);
+    ICMP_ECHO_REPLY* reply_buffer = new ICMP_ECHO_REPLY();
+    IcmpSendEcho(icmp_handle, (addr->S_un).S_addr, NULL, 0, NULL, reply_buffer, reply_size, timeout);
+    *reply = *reply_buffer;
+    return GetLastError();
+}
+
 int main()
 {
+    WSADATA wsa_startup_result;
+    WSAStartup(MAKEWORD(1, 1), &wsa_startup_result);
+
     const char* hostname = "google.com";
     char str_addr[100];
     in_addr *addr = new in_addr();
@@ -38,8 +51,13 @@ int main()
     if (err)
         return err;
 
+    ICMP_ECHO_REPLY* reply = new ICMP_ECHO_REPLY();
+    const int TIMEOUT = 5000;
+    ping(hostname, TIMEOUT, reply);
+
     inet_ntop(AF_INET, addr, str_addr, 100);
-    std::cout << "IP of " << hostname << ": " << str_addr << std::endl;
+    cout << "IP of " << hostname << ": " << str_addr << endl;
+    cout << "Time: " << reply->RoundTripTime << "ms, TTL: " << (int)(reply->Options.Ttl) << endl;
     WSACleanup();
     return 0;
 }
